736c4a585d658a0d0b985701dc81c7a524b16f3e
andstepko <andrew.stepko@gmail.com>
2016-05-27 00:00:27 +0300

    Some improvements, particularly auth token, server stopping and connection stopping.

--- main.cpp
+++ main.cpp
@@ -9,11 +9,15 @@
 #include <time.h>
 #include <sys/ipc.h>
 #include <sys/shm.h>
+#include <unistd.h>
+#include <signal.h>
 
 using namespace std;
 
 /* A simple server in the internet domain using TCP
 The port number is passed as an argument */
+
+char* TOKEN = "myToken";
 
 void error(const char *msg)
 {
@@ -32,7 +36,10 @@ int main(int argc, char *argv[])
         fprintf(stderr,"ERROR, no port provided\n");
         exit(1);
     }
+
+    // FIXME
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
+
     if (sockfd < 0)
         error("ERROR opening socket");
     bzero((char *) &serv_addr, sizeof(serv_addr));
@@ -40,6 +47,10 @@ int main(int argc, char *argv[])
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_addr.s_addr = INADDR_ANY;
     serv_addr.sin_port = htons(portno);
+
+    int option = 1;
+    setsockopt(sockfd, SOL_SOCKET, (SO_REUSEPORT | SO_REUSEADDR), (char*)&option, sizeof(option));
+
     if (bind(sockfd, (struct sockaddr *) &serv_addr,
              sizeof(serv_addr)) < 0)
         error("ERROR on binding");
@@ -51,17 +62,19 @@ int main(int argc, char *argv[])
     int shmid;
 
     int pid;
-    while (true) {
+    while (1) {
         newsockfd = accept(sockfd,
                            (struct sockaddr *) &cli_addr,
                            &clilen);
 
         pid = fork();
+        signal(SIGPIPE, SIG_IGN);
+
         if (pid < 0) {
             error("ERROR in new process creation");
         }
         if (pid != 0) {
-            //parent process
+            // Parent process
             shmid = shmget(SHARED_MEMORY_ID, SHARED_MEMORY_SIZE, 0666 | IPC_CREAT);
             bool * is_killed_ptr = (bool *)shmat(shmid, 0, 0);
             bool is_killed = *is_killed_ptr;
@@ -76,10 +89,10 @@ int main(int argc, char *argv[])
             continue;
         }
 
-        // child process
+        // Child process
         close(sockfd);
 
-        // check is killed
+        // Check is killed
         shmid = shmget(SHARED_MEMORY_ID, SHARED_MEMORY_SIZE, 0666 | IPC_CREAT);
         bool * is_killed_ptr = (bool *)shmat(shmid, 0, 0);
         bool is_killed = *is_killed_ptr;
@@ -93,31 +106,65 @@ int main(int argc, char *argv[])
         if (newsockfd < 0)
             error("ERROR on accept");
         bzero(buffer,256);
-        n = read(newsockfd,buffer,255);
-        if (n < 0) error("ERROR reading from socket");
 
-        printf("Here is the message: %s\n",buffer);
-        int response_len = 256 + 21;
-        char response[response_len];
-        bzero(response, response_len);
+        bool isFirstMessage = true;
+        while(1) {
+            printf("\nStarted reading...\n");
+            bzero(buffer, 256);
+//            n = read(newsockfd, buffer, 255);
+            n = recv(newsockfd, buffer, 255, MSG_NOSIGNAL);
 
-        time_t cur_time;
-        if (strncmp(buffer, "time", 4) == 0) {
-            printf("Responding with time");
-            time(&cur_time);
-            sprintf(response, "Current time is %ld\n", (long)cur_time);
-        } else if (strncmp(buffer, "kill", 4) == 0) {
-            printf("killing\n");
-            shmid = shmget(SHARED_MEMORY_ID, SHARED_MEMORY_SIZE, 0);
-            bool * is_killed = (bool *)shmat(shmid, 0, 0);
-            *(is_killed) = true;
-            shmdt(is_killed);
-        } else {
-            sprintf(response, "I got your message==>%s", buffer);
+            if (n < 0) {
+                error("ERROR reading from socket");
+                exit(1);
+            }
+
+            printf("Here is the message: %s\n", buffer);
+            int response_len = 256 + 21;
+            char response[response_len];
+            bzero(response, response_len);
+
+            if(isFirstMessage) {
+                printf("first message\n");
+                if (strncmp(buffer, TOKEN, strlen(TOKEN)) == 0) {
+                    printf("auth successful\n");
+                    isFirstMessage = false;
+                }
+                else{
+                    printf("auth ERROR!\n");
+                    close(newsockfd);
+                    return 1;
+                }
+            }
+
+            time_t cur_time;
+            if (strncmp(buffer, "time", 4) == 0) {
+                printf("responding with time\n");
+                time(&cur_time);
+                sprintf(response, "Current time is==>%ld\n", (long) cur_time);
+            } else if (strncmp(buffer, "exit", 4) == 0) {
+                printf("exiting\n");
+//                shmid = shmget(SHARED_MEMORY_ID, SHARED_MEMORY_SIZE, 0);
+//                bool *is_killed = (bool *) shmat(shmid, 0, 0);
+//                *(is_killed) = true;
+//                shmdt(is_killed);
+
+                close(newsockfd);
+                kill(getppid(), 9);
+                return 3;
+            } else if(strncmp(buffer, "kill", 4) == 0){
+                printf("killing\n");
+                close(newsockfd);
+                return 2;
+            }
+            else {
+                printf("responding with message\n");
+                sprintf(response, "I got your message==>%s", buffer);
+            }
+            n = send(newsockfd, response, response_len, MSG_NOSIGNAL);
+            if (n < 0) error("ERROR writing to socket");
         }
-        n = write(newsockfd, response, response_len);
-        if (n < 0) error("ERROR writing to socket");
-        close(newsockfd);
-        return 0;
+        //close(newsockfd);
+        //return 0;
     }
 }
